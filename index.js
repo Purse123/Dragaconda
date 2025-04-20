b34524abacb90ca6d40b3184368b7be0f03cbed9
Pierce Neupane <sahasneupane85@gmail.com>
2025-04-21 02:06:32 +0545

    updated initial tiles

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ let root = document.getElementById('root');
 const canvas = document.getElementById('mainCanvas');
 const ctx = canvas.getContext('2d');
 
-canvas.height = window.innerHeight;
-canvas.width = window.innerWidth;
+// canvas.height = window.innerHeight;
+// canvas.width = window.innerWidth;
+canvas.height = 500;
+canvas.width = 500;
 
 // pixel initialize
 const tileSize = 30;
@@ -13,13 +15,73 @@ const tiles = [];
 const no_cols = Math.floor(canvas.width / (tileSize + gap));
 const no_rows = Math.floor(canvas.height / (tileSize + gap));
 
-drawTiles();
-// dont need resize just for trial
-// window.addEventListener('resize', () => {console.log("you fucker"); drawTiles();});
+function RandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+let player = {
+    x: RandomInt(no_rows - 2) + 1,
+    y: RandomInt(no_cols - 2) + 1,
+
+    // color: "#A22929"           // red
+    // color: "#531880"           // violet
+    // color: "#3873E0"           // cyan
+    // color: "#27409C"           // dark blue
+    // color: "#A22974"           // pink
+    // color: "#832AF8"           // purple
+    color: "#D2B732"           // Yellow
+    // color: "#2DA635"           // light green
+    // color: "#1E7D29"           // dark green
+};
+
+// draw intial tiles
+function initialTile() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    
+    ctx.shadowColor = "#2D2926";
+    ctx.shadowOffsetX = 5;
+    ctx.shadowOffsetY = 5;
+
+    for (let y = 0; y < no_rows; y++) {
+	for (let x = 0; x < no_cols; x++) {
+            let px = x * (tileSize + gap);
+            let py = y * (tileSize + gap);
+
+            const dx = Math.abs(x - player.x);
+            const dy = Math.abs(y - player.y);
+
+            if (dx <= 1 && dy <= 1) {
+		ctx.fillStyle = player.color;
+            } else {
+		ctx.fillStyle = "#4E463F";
+            }
+
+            ctx.fillRect(px, py, tileSize, tileSize);
+	}
+    }
+}
+initialTile();
+
+function drawTiles() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    
+    ctx.shadowColor = "#2D2926";
+    ctx.shadowOffsetX = 5;
+    ctx.shadowOffsetY = 5;
+
+    for (let y = 0; y < no_rows; y++) {
+        for (let x = 0; x < no_cols; x++) {
+            let px = x * (tileSize + gap);
+            let py = y * (tileSize + gap);
+
+            ctx.fillStyle = "#4E463F";
+            ctx.fillRect(px, py, tileSize, tileSize);
+        }
+    }
+}
 
 // Drawing player
-// drawPlayer_circle();
-drawPlayer_rect();
+drawPlayer_circle(player);
 
 // player movement
 document.addEventListener("keydown", (e) => {
@@ -29,5 +91,5 @@ document.addEventListener("keydown", (e) => {
     if (e.key == "d") player.x++;
 
     drawTiles();
-    drawPlayer_rect();
+    drawPlayer_rect(player);
 });
--- player.js
+++ player.js
@@ -1,19 +1,4 @@
-// player
-let player = {
-    x: 3,
-    y: 5,
-    // color: "#A22929"           // red
-    // color: "#531880"           // violet
-    // color: "#3873E0"           // cyan
-    // color: "#27409C"           // dark blue
-    // color: "#A22974"           // pink
-    // color: "#832AF8"           // purple
-    color: "#D2B732"           // Yellow
-    // color: "#2DA635"           // light green
-    // color: "#1E7D29"           // dark green
-};
-
-function drawPlayer_rect() {
+function drawPlayer_rect(player) {
     px = player.x * (tileSize + gap);
     py = player.y * (tileSize + gap);
     ctx.fillStyle = player.color;
@@ -21,11 +6,12 @@ function drawPlayer_rect() {
     ctx.fillRect(px, py, tileSize, tileSize);
 }
 
-function drawPlayer_circle() {
+function drawPlayer_circle(player) {
     px = player.x * (tileSize + gap) + (50/100 * tileSize);
     py = player.y * (tileSize + gap) + (50/100 * tileSize);
     ctx.fillStyle = player.color;
     
-    ctx.arc(px, py, tileSize / 2, 0, Math.PI * 2);
+    ctx.arc(px, py, (tileSize + gap) / 2, 0, Math.PI * 2);
     ctx.fill();
 }
+
